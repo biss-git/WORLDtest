6a91622f6317996f0b9370e38120eaff325a02da
bisu <kotonohashimai@gmail.com>
2019-10-26 21:14:43 +0900

    test

---
+++ WORLDtest/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WORLDtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button_input = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_input
+            // 
+            this.button_input.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button_input.Location = new System.Drawing.Point(12, 12);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(204, 74);
+            this.button_input.TabIndex = 0;
+            this.button_input.Text = "ファイル入力";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.IsLogarithmic = true;
+            chartArea1.AxisY.Maximum = 1000D;
+            chartArea1.AxisY.Minimum = 100D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 92);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "ピッチ[Hz]";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 346);
+            this.chart1.SuppressExceptions = true;
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Location = new System.Drawing.Point(222, 42);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 34);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(222, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "フレーム数";
+            // 
+            // chart2
+            // 
+            chartArea2.AxisY.IsLogarithmic = true;
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(12, 455);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Legend = "Legend1";
+            series2.Name = "スペクトル包絡";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(776, 346);
+            this.chart2.SuppressExceptions = true;
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            // 
+            // button_save
+            // 
+            this.button_save.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button_save.Location = new System.Drawing.Point(609, 9);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(179, 67);
+            this.button_save.TabIndex = 5;
+            this.button_save.Text = "音声の保存";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 903);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Button button_save;
+    }
+}
+
---
+++ WORLDtest/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WORLD.NET;
+
+namespace WORLDtest
+{
+    public partial class Form1 : Form
+    {
+
+        // 読み込んだ音声のパラメータ
+        WorldParameters wp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            WorldConfig.init();
+        }
+
+        /// <summary>
+        /// ファイル読込を押したときの処理
+        /// </summary>
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            // 音声ファイル選択用のダイアログを表示
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "音声(.wav)|*.wav";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                // 音声分析
+                wp = WorldAnalysis.Analyse(ofd.FileName);
+                // ピッチをグラフに表示
+                chart1.Series[0].Points.Clear();
+                foreach(var f in wp.f0)
+                {
+                    chart1.Series[0].Points.Add(f);
+                }
+            }
+        }
+
+        /// <summary>
+        /// フレーム数が選択されたときの処理
+        /// </summary>
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if(wp == null || // 音声が読み込まれていない
+                numericUpDown1.Value > wp.f0_length) // 値が範囲外
+            {
+                // 何もしない
+                return;
+            }
+            // スペクトル包絡を表示
+            chart2.Series[0].Points.Clear();
+            int frameNumber = (int)numericUpDown1.Value;
+            float[] spectrogram = wp.spectrogram[frameNumber];
+            for (int i = 1; i < spectrogram.Length; i++)
+            {
+                chart2.Series[0].Points.AddXY(Math.Log(i), spectrogram[i]);
+            }
+        }
+
+        /// <summary>
+        /// 音声の保存ボタンが押されたときの処理
+        /// </summary>
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            if(wp == null) // 音声がない
+            {
+                // 何もしない
+                return;
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "音声(.wav)|*.wav";
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                // ピッチを半分にする
+                for(int i = 0; i < wp.f0_length; i++)
+                {
+                    wp.f0[i] /= 2;
+                }
+                // 保存する
+                Synthesizer synthesizer = new Synthesizer(wp);
+                synthesizer.SaveWav(sfd.FileName);
+            }
+        }
+    }
+}
